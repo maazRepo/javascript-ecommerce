91e9f454ed3b61a3023a7f740ee2dbb4e3e23edf
Maaz <81691403+maazRepo@users.noreply.github.com>
2021-05-12 03:20:32 +0200

    adding the secound slider, products slider

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ $(".slider-one")
     prevArrow: ".site-slider .slider-btn .prev",
     nextArrow: " .site-slider .slider-btn .next"
 });
+
+
+// Second slider
+
+$(".slider-two")
+.not(".slick-intialized")
+.slick({
+    slidesToShow:5,
+    slidesToScroll:1,
+    prevArrow: ".site-slider-two .prev",
+    nextArrow: " .site-slider-two .next",
+    autoplaySpeed:3000
+});
